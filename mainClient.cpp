cac8ee1b91df3b47ba4110079580e9f19c9e930b
AndresSalinas97 <i52salia@uco.es>
2019-10-06 11:54:38 +0200

    First commit

---
+++ mainClient.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file    mainClient.cpp
+ * @author  Andres Salinas Lima
+ * @date    07/10/2019
+ * @brief   Cliente para servidor TCP: Envia y recibe cadenas de texto
+ */
+
+
+#include <iostream>
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ mainServer.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file    mainServer.cpp
+ * @author  Andres Salinas Lima
+ * @date    07/10/2019
+ * @brief   Servidor TCP para el juego del Domino: Gestiona clientes, usuarios,
+ *          partidas y la logica del juego
+ */
+
+
+#include <iostream>
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
