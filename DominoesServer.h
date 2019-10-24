6648223354121fec5a1de80a93470ec5ca8175ab
AndresSalinas97 <i52salia@uco.es>
2019-10-24 12:37:51 +0200

    Document the DominoesServer class methods

--- DominoesServer.h
+++ DominoesServer.h
@@ -26,13 +26,22 @@ using std::string;
  * @class DominoesServer: clase para gestionar el servidor del juego del Dominó.
  */
 class DominoesServer {
-// TODO: Documentar miembros clase
 public:
+    /**
+     * Constructor: Crea un nuevo servidor que escuchará en el socket introducido.
+     */
     inline explicit DominoesServer(const Socket &serverSocket)
             : serverSocket(serverSocket) {};
 
+    /**
+     * Inicia el servidor.
+     * Función principal del servidor.
+     */
     void start();
 
+    /**
+     * Cierra el servidor después de avisar a los clientes.
+     */
     void end();
 
 private:
@@ -40,10 +49,22 @@ private:
     fd_set readFDS{};          // Set de descriptores para la función select()
     UsersManager usersManager; // Mánager de usuarios
 
+    /**
+     * Envía el mensaje introducido al cliente con el socketD introducido.
+     * @warning Se producirá un error si no hay un cliente conectado en el
+     * socketD introducido.
+     */
     static void sendMessage(int destinationSocketD, const char *message);
 
+    /**
+     * Acciones a realizar cuando se introduce texto por la entrada estándar
+     * (stdin) del programa servidor.
+     */
     void handleUserInput();
 
+    /**
+     * Acciones a realizar cuando un nuevo cliente se conecta al servidor.
+     */
     void handleNewClient();
 
     /**
@@ -51,31 +72,72 @@ private:
      */
     void handleGoneClient(int goneClientSocketD);
 
+    /**
+     * Acciones a realizar para eliminar a un cliente cuando este se desconecta.
+     */
     void removeClient(int goneClientSocketD);
 
+    /**
+     * Acciones a realizar cuando se recibe un mensaje de un cliente.
+     */
     void handleClientCommunication(int clientSocketD, const string &receivedMessage);
 
+    /**
+     * Imprime estadísticas en la salida estándar (stdout) del programa servidor.
+     */
     void printStats() const;
 
+    /**
+     * Imprime los comandos disponibles en la salida estándar (stdout) del
+     * programa servidor.
+     */
     static void printHelp();
 
+    /**
+     * Acciones a realizar cuando un cliente envía el comando USUARIO
+     */
     void handleUsuarioCommand(int clientSocketD, const string &username);
 
+    /**
+     * Acciones a realizar cuando un cliente envía el comando PASSWORD
+     */
     void handlePasswordCommand(int clientSocketD, const string &password);
 
+    /**
+     * Acciones a realizar cuando un cliente envía el comando REGISTRO
+     */
     void handleRegistroCommand(int clientSocketD, const string &username,
                                const string &password);
 
+    /**
+     * Acciones a realizar cuando un cliente envía el comando INICIAR-PARTIDA
+     */
     void handleIniciarPartidaCommand(int clientSocketD);
 
+    /**
+     * Acciones a realizar cuando un cliente envía el comando COLOCAR-FICHA
+     */
     void handleColocarFichaCommand(int clientSocketD, const string &dominoAndSide);
 
+    /**
+     * Acciones a realizar cuando un cliente envía el comando PASO-TURNO
+     */
     void handlePasoTurnoCommand(int clientSocketD);
 
+    /**
+     * Acciones a realizar cuando un cliente envía el comando ROBAR-FICHA
+     */
     void handleRobarFichaCommand(int clientSocketD);
 
+    /**
+     * Envía al cliente con el socketD introducido mensajes con los comandos
+     * que tiene disponibles.
+     */
     void sendHelp(int clientSocketD);
 
+    /**
+     * Acciones a realizar cuando un cliente envía el comando SALIR
+     */
     void handleSalirCommand(int clientSocketD);
 };
 
